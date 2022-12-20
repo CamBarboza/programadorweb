9d0e4306424dd8f0a3386f9a708a579a763a6ca1
CamBarboza <108905643+CamBarboza@users.noreply.github.com>
2022-12-20 15:56:06 -0300

    Delete M5U1 directory

--- M5U1/models/testimoniosModel.js
+++
@@ -1,9 +0,0 @@
-var pool = require('./bd');
-
-
-async function getTestimonios(){
-    var query = "select * from testimonios order by id desc";
-    var rows = await pool.query(query);
-    return rows;
-}
-module.exports = {getTestimonios}
