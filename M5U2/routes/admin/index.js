f4df00b7c740056e22311f169ecdf445a76056cc
CamBarboza <108905643+CamBarboza@users.noreply.github.com>
2022-12-24 14:13:43 -0300

    Add files via upload

---
+++ M5U2/routes/admin/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+var reservasModel = require('../../models/reservasModel')
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('admin/index', {
+    layout: 'admin/layout',
+  });
+});
+
+router.get('/', async function(req, res, next){
+    reservas = await reservasModel.getReservas();
+    reservas = reservas.splice(0,5);
+    res.render('admin/index',{
+        reservas
+    });
+});
+
+module.exports = router;
